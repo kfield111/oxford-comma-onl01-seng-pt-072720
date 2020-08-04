4fc7a585b17f004d1925780ad4170d696388779d
Kelin Field <kfield111@hotmail.com>
2020-08-04 00:25:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join (" and ")
   elsif array.length == 3
-    return "#{array[0}, #{array[1]}, and #{array[2]}"
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
   else array.join
   end
 
