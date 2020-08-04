ef4af3387642efd755d219f9e079706b04e7c3f4
Kelin Field <kfield111@hotmail.com>
2020-08-04 00:13:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length > 2
     array.join (", ")
   elsif array.length == 3
-    array.to_sentence    
+    array.to_sentence
   else array.join (" and ")
   end
 
